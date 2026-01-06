16b58c6c7ab40e6c8d3fa238854a931692ac5909
hamzaarafah6-star <hamzaarafah6@gmail.com>
2026-01-06 09:18:40 +0200

    program to check whether a number is divisible by 5 and 11 or not

---
+++ Divisible5and11.java
@@ -0,0 +1,14 @@
+package divisible5and11;
+import java.util.Scanner;
+public class Divisible5and11 {
+public static void main(String[] args) {
+Scanner n=new Scanner(System.in);        
+System.out.println("Enter your num");
+int x=n.nextInt();
+if (x%2==0)    
+System.out.println("your num even");
+else
+System.out.println("your num odd");    
+    }
+    
+}
